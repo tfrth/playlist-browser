986c82c1e8de7cccdd352135065dd27ea32e3f42
Tyler Frith <tfrith@sjc-tfrith-mba.local>
2015-07-30 16:17:58 -0700

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Tyler Frith on 7/30/15.
+//  Copyright (c) 2015 tfrth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Tyler Frith on 7/30/15.
+//  Copyright (c) 2015 tfrth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
